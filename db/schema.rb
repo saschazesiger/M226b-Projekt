e76b9bf4eee0fc9a641283c8552819ddec9d36b1
saschazesiger <janis.zesiger@gmail.com>
2024-01-03 12:21:06 +0000

    update shema

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_02_215037) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "username"
+    t.bigint "username"
     t.string "password_digest"
+    t.string "firstname"
+    t.string "surname"
+    t.string "email"
+    t.string "supervisor"
     t.datetime "lastlogin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "time_entries", "users"
