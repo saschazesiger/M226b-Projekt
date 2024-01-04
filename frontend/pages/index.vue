6ebefc0acbc4cad26703ef4fc15458123617821f
Janis <janis.zesiger@gmail.com>
2024-01-04 16:42:57 +0100

    Update

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,6 @@
 <template>
     <div>
-        <h2>Home</h2>
-        <p>Home page content goes here.</p>
+        <p class="text-primary">Home</p>
     </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style scoped>
-
-</style>
