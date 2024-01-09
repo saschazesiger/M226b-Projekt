3c30c356a1abde8e66adafd593dd0b0e822630f6
Janis <janis.zesiger@gmail.com>
2024-01-09 14:47:28 +0000

    new code

--- backend/db/migrate/20240102215037_create_time_entries.rb
+++ backend/db/migrate/20240102215037_create_time_entries.rb
@@ -3,8 +3,8 @@ class CreateTimeEntries < ActiveRecord::Migration[7.1]
     create_table :time_entries do |t|
       t.references :user, foreign_key: true
       t.datetime :time
-      t.boolean :action # 0 = in, 1 = out
-      t.boolean :edited # 0 = no, 1 = yes
+      t.boolean :action, default: false
+      t.boolean :edited, default: false
     end
   end
 end
